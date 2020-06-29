ecf8ee8f98eeae78692d829c1a96b5058ec476f5
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-28 22:10:09 -0300

    Primeiro Model e estruturação dos arquivos

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//Iniciando o App
+const app = express();
+
+//Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+requireDir('./src/models');
+
+ 
+//Redirecionando para o arquivo de rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
